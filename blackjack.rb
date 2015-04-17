3ce578fe7a22b7d8c2905308dbdfc935758953c0
Robin <robin-wagner@web.de>
2015-04-17 23:25:40 +0200

    Add first logic (first draft)

--- blackjack.rb
+++ blackjack.rb
@@ -1,1 +1,9 @@
 # BlackJack game
+#
+# Logic:
+# Bank deals two cards
+# Player decides to increase or pass
+# If increase and above 21 points => bank wins
+# If pass:
+#   Bank draws cards
+#   If above 21 points => player wins
